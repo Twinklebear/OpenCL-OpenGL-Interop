38bd890c3f622e8d6ad654ab7c7ac059b461a4f3
Will <willusher.life@gmail.com>
2013-04-22 13:59:01 -0700

    Now properly finding row, column and index values from the transpose block number

--- res/testtranspose.c
+++ res/testtranspose.c
@@ -58,9 +58,6 @@ void transpose(float *mat, int n){
 	//calculate row (i) and column (j) in a row major representation
 	int i = n % SIZE;
 	int j = n / SIZE;
-	// printf("i, j: %d, %d\n", i, j);
-	// printf("n: %d, computed n: %d\n", n, i + j * SIZE);
-	// printf("off diagonal compute of n: %d\n\n", i * SIZE + j);
 	//diagonal elements don't need any swapping
 	if (i == j)
 		return;
@@ -89,68 +86,31 @@ void transposeBlockN(float *mat, int n){
 	//by the # of blocks to the diagonal, ie if we're reading
 	//transpose block 2 in a 4x4 matrix [0-2] we should read 
 	//matrix block # 3
+	//Determine the matrix block number from the transpose block number
+	//and also find the row
 	int seenBlocks = blockPerRow;
-	//Transpose blocks per row
 	int tBlockPerRow = blockPerRow;
 	while (n >= seenBlocks){
 		seenBlocks += blockPerRow;
 		--tBlockPerRow;
 		row += BLOCK_SIZE;
 		n += blockPerRow - tBlockPerRow;
-		// printf("\trow: %d, tBlockPerRow: %d, n: %d\n", row, tBlockPerRow, n);
 	}
-	//Trying an idea
+	//Find the column
 	//n % tBlockPerRow * BLOCK_SIZE gives us the column relative to the diagonal
 	//block, which we then offset by the offset # of blocks
 	// (blockPerRow - tBlockPerRow) * BLOCK_SIZE
 	col = n % tBlockPerRow * BLOCK_SIZE + (blockPerRow - tBlockPerRow) * BLOCK_SIZE;
 
-	printf("\tmatrix block n: %d, row: %d, col: %d\n", n, row, col);
-	// if (n / blockPerRow == 0){
-	// 	n *= BLOCK_SIZE;
-	// 	printf("\tentry index: %d\n", n);
-	// }
-	// //n * BLOCK_SIZE is the initial col offset
-	// //n / blockPerRow * BLOCK_SIZE^2 is the block row/col offset?
-	// //(blockPerRow - tBlockPerRow) * BLOCK_SIZE is the current row offset
-	// else {
-	// 	//This only works for 6x6 with 2x2 block
-	// 	n = n * BLOCK_SIZE
-	// 		+ (n / blockPerRow) * (BLOCK_SIZE * BLOCK_SIZE) 
-	// 		+ (blockPerRow - tBlockPerRow) * BLOCK_SIZE;
-	// 	printf("\tn / blockPerRow > 0\n");
-	// 	printf("\tentry index: %d\n", n);
-	// }
-
-	// printf("\tsource location at n = %d\n", n);
+	printf("\tmatrix block #: %d, row: %d, col: %d\n", n, row, col);
+	int idx = row * SIZE + col;
+	printf("\t0, 0 of source location idx: %d\n", idx);
 	//Move float ptr to source block
-	//float *src = mat + n;
-	// printf("\tvalues:\n");
-	// for (int i = 0; i < BLOCK_SIZE; ++i){
-	// 	for (int j = 0; j < BLOCK_SIZE; ++j)
-	// 		printf("\t%*.2f", 5, src[i * SIZE + j]);
-	// 	printf("\n");
-	// }
-	/*
-	* on 4x4 they should be
-	* 0: 0, 0
-	* 1: 0, 2
-	* 2: 2, 2
-	*/
-	/*
-	* on the 6x6 block pos results SHOULD be
-	* 0: 0, 0
-	* 1: 0, 2
-	* 2: 0, 4
-	* 3: 2, 2
-	* 4: 2, 4
-	* 5: 4, 4
-	*/
+	float *src = mat + idx;
+	printf("\tvalues:\n");
+	for (int i = 0; i < BLOCK_SIZE; ++i){
+		for (int j = 0; j < BLOCK_SIZE; ++j)
+			printf("\t%*.2f", 5, src[i * SIZE + j]);
+		printf("\n");
+	}
 }
-/*
-* + 0 1 2 3
-* 0 a b c d
-* 1 e f g h
-* 2 i j k l
-* 3 m n o p
-*/
